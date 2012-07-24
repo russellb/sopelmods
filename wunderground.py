611a951f3c51e00cb18b4999db6b1353a45369b4
Juggie <donnyk@gmail.com>
2012-07-24 14:34:02 -0400

    -Add check for 'error' when city is invalid -Add check for multiple cities returned and list cities -Make almanac output only if the information is provided -Output forecast URL that can be clicked on

--- wunderground.py
+++ wunderground.py
@@ -2,68 +2,101 @@ import urllib
 import json
 
 def wuweather(phenny, input):
-    loc = input.group(2)
+	#loc = input.group(2)
+	loc = input
 
-    #grab weather from api
-    try:
-        weather = json.loads(urllib.urlopen('http://api.wunderground.com/api/587a76efcc30bee2/conditions/almanac/astronomy/forecast/pws:0/q/' + loc +'.json').read())
-    except:
-        return
-    if not weather:
-        return
+	#grab weather from api
+	try:
+		weather = json.loads(urllib.urlopen('http://api.wunderground.com/api/587a76efcc30bee2/conditions/almanac/astronomy/forecast/pws:0/q/' + loc +'.json').read())
+	except:
+		return
+	if not weather:
+		return
 
-    try:
-        #format the city name
-        city = "[%s, %s, %s]" % \
-        (weather['current_observation']['display_location']['city'],
-        weather['current_observation']['display_location']['state'],
-        weather['current_observation']['display_location']['country'])
+	#does the response contain an error
+	if 'error' in weather['response']:
+		phenny.say("wuweather: %s" % weather['response']['error']['description'])
+		return
+	#does the response contain extra 'results'
+	elif 'results' in weather['response']:
+		output = 'wuweather: Ambigious Search, options: '
+		counter = 0
+		for index in weather['response']['results']:
+			if counter == 0:
+				output += "%s %s %s " % (index['city'], index['state'], index['country'])
+			else:
+				output += "/ %s %s %s " % (index['city'], index['state'], index['country'])				
+			counter += 1
+		phenny.say(output)
+		return
 
-        #output the averages, sunrise, moon
-        output = "%s Avg High: %sF, %sC Low: %sF, %sC / Sunrise: %s:%s Sunset: %s:%s / Moon: %s%%\n" % \
-        (city,
-        weather['almanac']['temp_high']['normal']['F'],
-        weather['almanac']['temp_high']['normal']['C'],
-        weather['almanac']['temp_low']['normal']['F'],
-        weather['almanac']['temp_low']['normal']['C'],
-        weather['moon_phase']['sunrise']['hour'],
-        weather['moon_phase']['sunrise']['minute'],
-        weather['moon_phase']['sunset']['hour'],
-        weather['moon_phase']['sunset']['minute'],
-        weather['moon_phase']['percentIlluminated'])
+	#if we made it here, everything should be good
+	try:
+		#format the city name
+		city = "[%s, %s, %s]" % \
+		(weather['current_observation']['display_location']['city'],
+		weather['current_observation']['display_location']['state'],
+		weather['current_observation']['display_location']['country'])
 
-        #output the current weather
-        output += "%s Current: %s %sF, %sC Humidity: %s, Wind: %s\n" % \
-        (city,
-        weather['current_observation']['weather'],
-        weather['current_observation']['temp_f'],
-        weather['current_observation']['temp_c'],
-        weather['current_observation']['relative_humidity'],
-        weather['current_observation']['wind_string'])
+		#output the averages, sunrise, moon
+		#some locations don't have an almanac
+		if weather['almanac']['airport_code'] != "":
+			almanac_output = "Avg High: %sF, %sC Low: %sF, %sC " % \
+			(weather['almanac']['temp_high']['normal']['F'],
+			weather['almanac']['temp_high']['normal']['C'],
+			weather['almanac']['temp_low']['normal']['F'],
+			weather['almanac']['temp_low']['normal']['C'])
+		else:
+			almanac_output = ""
 
-        #output first 3 days of forcast (today, tomorrow, second day)
-        counter = 0
-        for index in weather['forecast']['simpleforecast']['forecastday']:
-            counter +=1
-            output += "%s %s: %s [Chance of Percipitation: %s%%] [High: %sF, %sC Low: %sF, %sC] Humidity: [Max: %s, Min: %s, Avg: %s] Wind: [%smph, %skph]\n" % \
-            (city,
-            index['date']['weekday'],
-            index['conditions'],
-            index['pop'],
-            index['high']['fahrenheit'],
-            index['high']['celsius'],
-            index['low']['fahrenheit'],
-            index['low']['celsius'],
-            index['maxhumidity'],
-            index['minhumidity'],
-            index['avehumidity'],
-            index['avewind']['mph'],
-            index['avewind']['kph'])
-            if counter >= 2: break
-    except:
-        return
+		#output sunrise w/ almanac if available
+		phenny.say("%s %sSunrise: %s:%s Sunset: %s:%s" % \
+		(city,
+		almanac_output,
+		weather['moon_phase']['sunrise']['hour'],
+		weather['moon_phase']['sunrise']['minute'],
+		weather['moon_phase']['sunset']['hour'],
+		weather['moon_phase']['sunset']['minute']))
 
-    #print output
-    phenny.say(output)
 
-wuweather.commands = ['wuweather']
+		#output the current weather
+		phenny.say("%s Current: %s %sF, %sC Humidity: %s, Wind: %s" % \
+		(city,
+		weather['current_observation']['weather'],
+		weather['current_observation']['temp_f'],
+		weather['current_observation']['temp_c'],
+		weather['current_observation']['relative_humidity'],
+		weather['current_observation']['wind_string']))
+
+		#output first 3 days of forcast (today, tomorrow, second day)
+		counter = 0
+		for index in weather['forecast']['simpleforecast']['forecastday']:
+			if counter == 0:
+				day = 'Today'
+			elif counter == 1:
+				day = 'Tomorrow'
+			else:
+				day = index['date']['weekday']
+
+			phenny.say("%s %s: %s [POP: %s%%] [High: %sF, %sC Low: %sF, %sC] Humidity: [Max: %s%%, Min: %s%%, Avg: %s%%] Wind: [%smph, %skph]" % \
+			(city,
+			day,
+			index['conditions'],
+			index['pop'],
+			index['high']['fahrenheit'],
+			index['high']['celsius'],
+			index['low']['fahrenheit'],
+			index['low']['celsius'],
+			index['maxhumidity'],
+			index['minhumidity'],
+			index['avehumidity'],
+			index['avewind']['mph'],
+			index['avewind']['kph']))
+			counter +=1
+			if counter >= 3: break
+		
+		phenny.say("%s URL: %s" % (city, weather['current_observation']['forecast_url']))
+	except:
+		return
+
+gweather.commands = ['wuweather']
