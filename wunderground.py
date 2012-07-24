ab06c4bd13a47e8d4021ff6ae60659c1846c8203
juggie <donnyk@gmail.com>
2012-07-24 12:05:54 -0300

    Update wunderground.py

--- wunderground.py
+++ wunderground.py
@@ -20,7 +20,7 @@ def wuweather(phenny, input):
         weather['current_observation']['display_location']['country'])
 
         #output the averages, sunrise, moon
-        output = "%s Avg High: %sF, %sC Low: %sF, %sC / Sunrise: %s:%s Sunset: %s:%s / Moon: %s%%\n" % \
+        phenny.say("%s Avg High: %sF, %sC Low: %sF, %sC / Sunrise: %s:%s Sunset: %s:%s / Moon: %s%%\n" % \
         (city,
         weather['almanac']['temp_high']['normal']['F'],
         weather['almanac']['temp_high']['normal']['C'],
@@ -30,22 +30,22 @@ def wuweather(phenny, input):
         weather['moon_phase']['sunrise']['minute'],
         weather['moon_phase']['sunset']['hour'],
         weather['moon_phase']['sunset']['minute'],
-        weather['moon_phase']['percentIlluminated'])
+        weather['moon_phase']['percentIlluminated']))
 
         #output the current weather
-        output += "%s Current: %s %sF, %sC Humidity: %s, Wind: %s\n" % \
+        phenny.say("%s Current: %s %sF, %sC Humidity: %s, Wind: %s\n" % \
         (city,
         weather['current_observation']['weather'],
         weather['current_observation']['temp_f'],
         weather['current_observation']['temp_c'],
         weather['current_observation']['relative_humidity'],
-        weather['current_observation']['wind_string'])
+        weather['current_observation']['wind_string']))
 
         #output first 3 days of forcast (today, tomorrow, second day)
         counter = 0
         for index in weather['forecast']['simpleforecast']['forecastday']:
             counter +=1
-            output += "%s %s: %s [Chance of Percipitation: %s%%] [High: %sF, %sC Low: %sF, %sC] Humidity: [Max: %s, Min: %s, Avg: %s] Wind: [%smph, %skph]\n" % \
+            phenny.say("%s %s: %s [Chance of Percipitation: %s%%] [High: %sF, %sC Low: %sF, %sC] Humidity: [Max: %s, Min: %s, Avg: %s] Wind: [%smph, %skph]\n" % \
             (city,
             index['date']['weekday'],
             index['conditions'],
@@ -58,12 +58,9 @@ def wuweather(phenny, input):
             index['minhumidity'],
             index['avehumidity'],
             index['avewind']['mph'],
-            index['avewind']['kph'])
+            index['avewind']['kph']))
             if counter >= 2: break
     except:
         return
-
-    #print output
-    phenny.say(output)
 
 wuweather.commands = ['wuweather']
