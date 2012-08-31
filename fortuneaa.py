4d3b507a983490765bfd06066d0875c1ab91f850
Russell Bryant <russell@russellbryant.net>
2012-08-31 12:35:21 -0300

    Update fortuneaa.py
    Fix typo.

--- fortuneaa.py
+++ fortuneaa.py
@@ -3,6 +3,6 @@ import commands
 def fortuneaa(phenny, input):
    #fortune = commands.getoutput('fortune ascii-art mario.arteascii')
    fortune = commands.getoutput('fortune ascii-art')
-   for line in fortune.rsplit('\n')
+   for line in fortune.rsplit('\n'):
       phenny.say(line)
 fortuneaa.commands = ['fortuneaa']
