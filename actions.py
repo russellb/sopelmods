d994f4aa62a5ab18c101a74ec2c4501e15c4aaba
Russell Bryant <russell@russellbryant.net>
2012-09-17 15:59:22 -0400

    Put the table back.

--- actions.py
+++ actions.py
@@ -4,6 +4,12 @@ def table(phenny, input):
    phenny.say('(╯°□°）╯︵ ┻━┻')
 table.commands = ['table', 'rage']
 
+
+def untable(phenny, input):
+   phenny.say('┬┬ ノ(゜-゜ノ)')
+untable.commands = ['untable', 'putitback', 'unrage']
+
+
 def joyful(phenny, input):
    joyful_emotes = [ '🙌', '😀', '😁', '😂', '😃', '😄', '😅' ]
    from random import choice
