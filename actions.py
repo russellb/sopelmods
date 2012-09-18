0fa316f0c94e4e7ff955d91bd7975b1d5cedcb0c
Russell Bryant <russell@russellbryant.net>
2012-09-18 13:03:49 -0400

    pep8 related cleanup, no real changes

--- actions.py
+++ actions.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
 
+from random import choice
+
+
 def table(phenny, input):
-   phenny.say('(╯°□°）╯︵ ┻━┻')
+    phenny.say('(╯°□°）╯︵ ┻━┻')
 table.commands = ['table', 'rage']
 
 
 def untable(phenny, input):
-   phenny.say('┬┬ ノ(゜-゜ノ)')
+    phenny.say('┬┬ ノ(゜-゜ノ)')
 untable.commands = ['untable', 'putitback', 'unrage']
 
 
 def joyful(phenny, input):
-   joyful_emotes = [ '🙌', '😀', '😁', '😂', '😃', '😄', '😅' ]
-   from random import choice
-   phenny.say(choice(joyful_emotes))
-joyful.commands = [ 'huzzuh', 'awesome', 'happy', 'smile' ]
+    joyful_emotes = ['🙌', '😀', '😁', '😂', '😃', '😄', '😅']
+    phenny.say(choice(joyful_emotes))
+joyful.commands = ['huzzuh', 'awesome', 'happy', 'smile']
