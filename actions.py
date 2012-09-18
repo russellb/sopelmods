74470bfdae9da4af651a623fb0f6db441ae991fd
Russell Bryant <russell@russellbryant.net>
2012-09-18 13:13:21 -0400

    Add finger and umadbro

--- actions.py
+++ actions.py
@@ -17,3 +17,14 @@ def joyful(phenny, input):
     joyful_emotes = ['🙌', '😀', '😁', '😂', '😃', '😄', '😅']
     phenny.say(choice(joyful_emotes))
 joyful.commands = ['huzzuh', 'awesome', 'happy', 'smile']
+
+
+def finger(phenny, input):
+    fingers = ['┌∩┐(◣_◢)┌∩┐', '凸(¬‿¬)凸']
+    phenny.say(choice(fingers))
+finger.commands = ['finger']
+
+
+def umadbro(phenny, input):
+    phenny.say('¯\_(ツ)_/¯')
+umadbro.commands = ['umadbro']
