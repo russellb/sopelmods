1b90f892e4f26c2e32fa7ec7a8e0fce1953654d8
Russell Bryant <russell@russellbryant.net>
2013-09-06 10:52:22 -0400

    Add devexcuse

---
+++ devexcuse.py
@@ -0,0 +1,11 @@
+import urllib
+from xml.dom.minidom import parseString
+
+def devexcuse(phenny, input):
+    phenny.say(parseString(
+        urllib.urlopen('http://developerexcuses.com').read().replace('&', '')).
+        getElementsByTagName('body')[0].getElementsByTagName('div')[0].
+        getElementsByTagName('center')[0].getElementsByTagName('a')[0].
+        childNodes[0].nodeValue)
+devexcuse.commands = ['devexcuse']
+
