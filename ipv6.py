2f838f94c1fd8fdada176b3fd3ce66b72edd0ede
Leif Madsen <lmadsen@redhat.com>
2016-05-26 10:15:41 -0400

    Strip a href stuff

--- ipv6.py
+++ ipv6.py
@@ -4,8 +4,8 @@ import urllib
 
 def ipv6(phenny, input):
     try:
-        phenny.say(re.search('<h1>(.*)',
-                urllib.urlopen('http://ipv6excuses.com').read()).group(1))
+        s = re.search('<h1>(.*)', urllib.urlopen('http://ipv6excuses.com').read()).group(1)
+        phenny.say(re.sub('<.*?>', '', s))
     except Exception:
         pass
 ipv6.commands = ['ipv6', 'ipv6excuse']
