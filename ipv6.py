ca651b2d7b7c92583e1d12e63b870408b2cf56c4
Leif Madsen <lmadsen@redhat.com>
2016-05-26 11:12:31 -0400

    Add link back in if found

--- ipv6.py
+++ ipv6.py
@@ -5,7 +5,13 @@ import urllib
 def ipv6(phenny, input):
     try:
         s = re.search('<h1>(.*)', urllib.urlopen('http://ipv6excuses.com').read()).group(1)
-        phenny.say(re.sub('<.*?>', '', s))
+        urls = re.findall(r'href=[\'"]?([^\'" >]+)', s)
+        quote = re.sub('<.*?>', '', s)
+
+        if urls:
+            quote = quote + " (" + ', '.join(urls) + ")"
+
+        phenny.say(quote)
     except Exception:
         pass
 ipv6.commands = ['ipv6', 'ipv6excuse']
